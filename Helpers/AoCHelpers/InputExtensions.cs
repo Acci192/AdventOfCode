dd436a3b041a7783122618e78cf1c3f05da4c56c
Andreas Karlsson <k.andreas@live.se>
2022-12-04 19:50:47 +0100

    Added Helper Project

---
+++ Helpers/AoCHelpers/InputExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace AoCHelpers;
+
+public static class InputExtensions
+{
+    public static Dictionary<(int x, int y), char> ToCoordinateDictionary(this IEnumerable<string> input)
+    {
+        var result = new Dictionary<(int x, int y), char>();
+
+        foreach(var (row, y) in input.Select((x, i) => (x, i)))
+        {
+            foreach(var (c, x) in row.Select((x, i) => (x, i)))
+            {
+                result.Add((x, y), c);
+            }
+        }
+
+        return result;
+    }
+
+}
