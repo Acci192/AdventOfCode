af06e4f77e4e4138e0139aa4d8db4eaac0f7bfb9
Andreas Karlsson <k.andreas@live.se>
2022-12-04 20:29:57 +0100

    Added a Generator to generate a number of positions with a delta

---
+++ Helpers/AoCHelpers/SequenceGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCHelpers;
+
+public static class SequenceGenerator
+{
+    public static IEnumerable<(int x, int y)> GeneratePositions(int numberOfPositions, int xStart, int yStart, int xDelta, int yDelta, bool includeStart = false)
+    {
+        if(includeStart)
+            yield return (xStart, yStart);
+
+        for(var i = 1; i <= numberOfPositions; i++)
+        {
+            yield return (xStart + (xDelta * i), yStart + (yDelta * i));
+        }
+    }
+}
