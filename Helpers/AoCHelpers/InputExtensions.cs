666cdb2a23490d13998feca94d62f42a52207194
Andreas Karlsson <k.andreas@live.se>
2022-12-04 19:57:19 +0100

    Added overload to ToCoordinateDictionary

--- Helpers/AoCHelpers/InputExtensions.cs
+++ Helpers/AoCHelpers/InputExtensions.cs
@@ -4,17 +4,24 @@ public static class InputExtensions
 {
     public static Dictionary<(int x, int y), char> ToCoordinateDictionary(this IEnumerable<string> input)
     {
+        return input.ToCoordinateDictionary(c => true);
+    }
+
+    public static Dictionary<(int x, int y), char> ToCoordinateDictionary(this IEnumerable<string> input, Func<char, bool> predicate)
+    {
         var result = new Dictionary<(int x, int y), char>();
 
-        foreach(var (row, y) in input.Select((x, i) => (x, i)))
+        foreach (var (row, y) in input.Select((x, i) => (x, i)))
         {
-            foreach(var (c, x) in row.Select((x, i) => (x, i)))
+            foreach (var (c, x) in row.Select((x, i) => (x, i)))
             {
-                result.Add((x, y), c);
+                if (predicate(c))
+                {
+                    result.Add((x, y), c);
+                }
             }
         }
 
         return result;
     }
-
 }
